fc35830b393b14d0e55dbe916722a2d19699a63f
thinh <trungthinh.99@gmail.com>
2025-03-10 19:51:37 +0700

    implement UI bulletin

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,42 @@
 import 'react-toastify/dist/ReactToastify.css';
-import React, { FunctionComponent, useState } from 'react';
+import React, { useState } from 'react';
 import { Post } from './type/post';
 import usePostStore from './hook/use-post-store';
 import { ToastContainer, toast } from 'react-toastify';
+import {
+  Backdrop,
+  Box,
+  Button,
+  Card,
+  Container,
+  Divider,
+  Fade,
+  Modal,
+  Stack,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { LABEL } from './constants/label';
+import Grid from '@mui/material/Grid2';
 
 const App: React.FC = () => {
   const { posts, addPost, updatePost, deletePost } = usePostStore();
   const [title, setTitle] = useState<string>('');
   const [content, setContent] = useState<string>('');
   const [editingPost, setEditingPost] = useState<Post | null>(null);
+  const [confirmDelete, setConfirmDelete] = useState<{ isDelete: boolean; id?: number }>({
+    isDelete: false,
+  });
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     if (editingPost) {
       updatePost(editingPost.id, title, content);
+      toast.info(LABEL.noti.update_success);
       setEditingPost(null);
     } else {
       addPost(title, content);
-      toast.success('Post added successfully!');
+      toast.success(LABEL.noti.create_success);
     }
     setTitle('');
     setContent('');
@@ -30,69 +49,223 @@ const App: React.FC = () => {
   };
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Bulletin Board</h1>
+    <Container sx={{ backgroundColor: '#f5f7fa' }} maxWidth="lg">
+      <Typography
+        component="h1"
+        variant="h1"
+        textAlign="center"
+        mb={4}
+        mt={2}
+        sx={{ fontWeight: 700, fontSize: 50, color: '#2c3e50' }}
+      >
+        {LABEL.bulletin_board}
+      </Typography>
 
-      <ToastContainer position="top-right" autoClose={3000} />
+      <ToastContainer position="top-right" autoClose={1800} />
 
-      <form onSubmit={handleSubmit} className="mb-4">
-        <input
-          type="text"
-          placeholder="Title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          className="border p-2 mr-2"
-          required
+      <Grid container spacing={3} sx={{ justifyContent: 'center' }}>
+        <Grid size={{ xs: 12, md: 5.8 }}>
+          <form onSubmit={handleSubmit}>
+            <Card sx={{ p: 4, py: 3, borderRadius: 2 }}>
+              <Stack gap={2}>
+                <Box>
+                  <Typography sx={{ fontWeight: 700, color: '#2c3e50', fontSize: 20 }}>
+                    {editingPost ? LABEL.update_title : LABEL.create_title}
+                  </Typography>
+                  <Divider sx={{ mt: 1, borderWidth: 1 }} />
+                </Box>
+                <Box>
+                  <Typography sx={{ fontWeight: 600, color: '#2c3e50', fontSize: 16, mb: 0.5 }}>
+                    {LABEL.title_form} *
+                  </Typography>
+                  <TextField
+                    size="small"
+                    placeholder={LABEL.placeholder.title}
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    fullWidth
+                    required
+                  />
+                </Box>
+                <Box>
+                  <Typography sx={{ fontWeight: 600, color: '#2c3e50', fontSize: 16, mb: 0.5 }}>
+                    {LABEL.desc_form} *
+                  </Typography>
+                  <TextField
+                    size="small"
+                    placeholder={LABEL.placeholder.desc}
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                    fullWidth
+                    multiline
+                    rows={5}
+                    required
+                  />
+                </Box>
+                <Stack direction="row" spacing={1}>
+                  <Button variant="contained" fullWidth type="submit" sx={{ borderRadius: '6px' }}>
+                    {editingPost ? LABEL.button_label.update : LABEL.button_label.create}
+                  </Button>
+                  {editingPost && (
+                    <Button
+                      onClick={() => {
+                        setEditingPost(null);
+                        setTitle('');
+                        setContent('');
+                      }}
+                      sx={{ borderRadius: '6px' }}
+                      variant="contained"
+                      color="inherit"
+                      fullWidth
+                    >
+                      {LABEL.button_label.cancel}
+                    </Button>
+                  )}
+                </Stack>
+              </Stack>
+            </Card>
+          </form>
+        </Grid>
+
+        <Divider
+          orientation="vertical"
+          flexItem
+          sx={{
+            display: { xs: 'none', md: 'block' },
+            borderWidth: 1.5,
+            borderColor: '#86a8e7',
+          }}
         />
-        <input
-          type="text"
-          placeholder="Content"
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-          className="border p-2 mr-2"
-          required
-        />
-        <button type="submit" className="bg-blue-500 text-black p-2">
-          {editingPost ? 'Update' : 'Post'}
-        </button>
-        {editingPost && (
-          <button
-            onClick={() => {
-              setEditingPost(null);
-              setTitle('');
-              setContent('');
+
+        <Grid size={{ xs: 12, md: 5.8 }}>
+          <Stack gap={2}>
+            <Box>
+              <Typography sx={{ fontWeight: 700, color: '#2c3e50', fontSize: 20 }}>
+                {LABEL.bulletin_list}
+              </Typography>
+              <Divider sx={{ mt: 1, borderWidth: 1 }} />
+            </Box>
+            {posts.length === 0 && (
+              <Typography sx={{ fontWeight: 400, color: 'gray', fontSize: 16 }}>
+                {LABEL.no_bulletin}
+              </Typography>
+            )}
+            <Stack>
+              {posts.map((post) => (
+                <Card
+                  sx={{
+                    p: 2,
+                    py: 1.5,
+                    borderRadius: 2,
+                    ':hover': {
+                      transform: 'translateY(-3px)',
+                      boxShadow: '0 4px 12px rgba(0,0,0,0.05)',
+                    },
+                    transition: 'transform 0.2s, box-shadow 0.2s',
+                    mb: '12px',
+                  }}
+                  key={post.id}
+                >
+                  <Typography sx={{ fontWeight: 700, color: '#2c3e50', fontSize: 18 }}>
+                    {post.title}
+                  </Typography>
+                  <Typography sx={{ color: '#555', fontSize: 20 }}>{post.content}</Typography>
+                  <Stack direction="row" justifyContent="flex-end" mt={1} spacing={1}>
+                    <Button
+                      variant="contained"
+                      color="warning"
+                      onClick={() => handleEdit(post)}
+                      sx={{
+                        textTransform: 'none',
+                        borderRadius: '6px',
+                        backgroundColor: '#f0ad4e',
+                      }}
+                    >
+                      {LABEL.button_label.edit}
+                    </Button>
+                    <Button
+                      variant="contained"
+                      color="error"
+                      onClick={() => setConfirmDelete({ isDelete: true, id: post.id })}
+                      sx={{ textTransform: 'none', borderRadius: '6px' }}
+                    >
+                      {LABEL.button_label.delete}
+                    </Button>
+                  </Stack>
+                </Card>
+              ))}
+            </Stack>
+          </Stack>
+        </Grid>
+      </Grid>
+
+      <Modal
+        open={confirmDelete.isDelete}
+        onClose={() => setConfirmDelete({ isDelete: false })}
+        closeAfterTransition
+        slots={{ backdrop: Backdrop }}
+        slotProps={{
+          backdrop: {
+            timeout: 500,
+          },
+        }}
+      >
+        <Fade in={confirmDelete.isDelete}>
+          <Box
+            sx={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              width: 400,
+              bgcolor: 'background.paper',
+              borderRadius: '6px',
+              boxShadow: 24,
+              p: 2.5,
             }}
-            className="bg-gray-500 text-black p-2 ml-2"
           >
-            Cancel
-          </button>
-        )}
-      </form>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id} className="border p-2 mb-2 flex justify-between">
-            <div>
-              <h2 className="font-bold">{post.title}</h2>
-              <p>{post.content}</p>
-            </div>
-            <div>
-              <button
-                onClick={() => handleEdit(post)}
-                className="px-3 py-1 mr-3 bg-yellow-500 text-black rounded-md shadow-md hover:bg-yellow-600 transition-all"
+            <Typography
+              variant="h6"
+              component="h2"
+              textAlign="center"
+              fontWeight={600}
+              color="error"
+              textTransform={'uppercase'}
+            >
+              {LABEL.delete_modal.title}
+            </Typography>
+            <Typography textAlign="center" sx={{ mt: 1 }}>
+              {LABEL.delete_modal.confirm_message}
+            </Typography>
+            <Typography textAlign="center" sx={{ mt: 1 }}>
+              {LABEL.delete_modal.cannot_restore}
+            </Typography>
+            <Stack direction="row" spacing={2} mt={2} justifyContent="center">
+              <Button
+                variant="contained"
+                color="error"
+                onClick={() => {
+                  deletePost(confirmDelete?.id || 0);
+                  setConfirmDelete({ isDelete: false });
+                  toast.error(LABEL.noti.delete_success);
+                }}
+                sx={{ textTransform: 'none', borderRadius: '6px' }}
               >
-                Edit
-              </button>
-              <button
-                onClick={() => deletePost(post.id)}
-                className="px-3 py-1 bg-red-500 text-black rounded-md shadow-md hover:bg-red-600 transition-all"
+                {LABEL.button_label.yes}
+              </Button>
+              <Button
+                variant="contained"
+                color="inherit"
+                onClick={() => setConfirmDelete({ isDelete: false })}
+                sx={{ textTransform: 'none', borderRadius: '6px' }}
               >
-                Delete
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
+                {LABEL.button_label.no}
+              </Button>
+            </Stack>
+          </Box>
+        </Fade>
+      </Modal>
+    </Container>
   );
 };
 
---
+++ src/constants/label.ts
@@ -0,0 +1,32 @@
+export const LABEL = {
+  title_form: 'Title',
+  desc_form: 'Description',
+  create_title: 'Create bulletin',
+  update_title: 'Update bulletin',
+  bulletin_board: 'Bulletin Board',
+  bulletin_list: 'Bulletin List',
+  no_bulletin: 'Empty list',
+  placeholder: {
+    title: 'Please enter title...',
+    desc: 'Please enter description...',
+  },
+  button_label: {
+    update: 'Update',
+    create: 'Create',
+    cancel: 'Cancel',
+    edit: 'Edit',
+    delete: 'Delete',
+    yes: 'Yes',
+    no: 'No',
+  },
+  noti: {
+    create_success: 'Create bulletin successfully!',
+    update_success: 'Update bulletin successfully!',
+    delete_success: 'Bulletin has been deleted!',
+  },
+  delete_modal: {
+    title: 'Delete bulletin',
+    confirm_message: 'Are you sure to delete?',
+    cannot_restore: 'This bulletin can not restore after click Yes',
+  },
+};
